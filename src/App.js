3665b556f38f5ecb3339a13a532f6ec70620f781
Mathew Chaney <mathew.chaney@cloudreach.com>
2019-11-05 10:27:01 -0500

    add inventory rendering

--- src/App.js
+++ src/App.js
@@ -25,17 +25,16 @@ const App = () => {
   );
 }
 
-const HeroList = ({heroes, currentHero, setCurrentHero}) => {
-  const hero = heroes[currentHero];
+const HeroList = ({heroes, setCurrentHero}) => {
   return (
      <div className="heroList">
       <div className="sidebarHeader">
-        <span>Heroes</span>
+        <span>HEROES</span>
         <span className="addHero">
           <AddBox/>
         </span>
       </div>
-      {heroes.map(hero => 
+      {heroes && heroes.map(hero => 
         <HeroSummary
             key={hero.name}
             hero={hero} 
@@ -57,9 +56,44 @@ const HeroSummary = ({hero, setCurrentHero}) => {
 const HeroContent = ({hero, setCurrentHero}) => {
   return (
     <div className="heroCard">
-      {hero.name}, Level {hero.level} {hero.characterClass}
+      <HeroStatus hero={hero} />
+      <Inventory inventory={hero.inventory} />
     </div>
   )
+}
+
+const HeroStatus = ({hero}) => {
+  return (
+    <div>
+      <h2>Status</h2><hr />
+      {hero.name}, Level {hero.level} {hero.characterClass}
+    </div>
+  );
+}
+
+const Inventory = ({inventory}) => {
+  return (
+    <div>
+      <h2>Inventory</h2><hr />
+      {inventory && inventory.map((item) => <Item item={item}/>)}
+    </div>
+  );
+}
+
+const Item = ({item}) => {
+  return (
+    <div className="item">
+      {item.count}x {item.name} {item.weight*item.count}{item.weightUnit}
+      {item.extra && 
+        Object.keys(item.extra).map((key) => {
+          return (
+            <div className="extra">
+              <span className="filler"/>{key}: {item.extra[key]}
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default App;
--- src/defaultHeroes.js
+++ src/defaultHeroes.js
@@ -3,7 +3,19 @@ const defaultHeroes = [
   {
     name: "Rygan",
     characterClass: "Barbarian",
-    level: 5
+    level: 5,
+    inventory: [
+      {
+        name: "Greataxe",
+        count: 1,
+        weight: 10,
+        weightUnit: "lb",
+        extra: {
+          attackAttribute: "str",
+          damageDice: "1d12"
+        }
+      }
+    ]
   },
   {
     name: "Diman",
@@ -12,30 +24,65 @@ const defaultHeroes = [
     inventory: [
       {
         name: "Wizard's Spellbook",
-        weight: "2 lb"
+        count: 1,
+        weight: 2,
+        weightUnit: "lb"
       },
       {
         name: "Wand of Magic Missile",
-        weight: "0.1 lb"
+        count: 1,
+        weight: 0.1,
+        weightUnit: "lb",
+        extra: {
+          wandCharges: 7,
+          wandChargeRefreshDice: "1d6+1"
+        }
       },
       {
         name: "Wooden Staff",
-        weight: "5 lb",
-        damageDice: "1d6"
+        count: 1,
+        weight: 5,
+        weightUnit: "lb",
+        extra: {
+          attackAttribute: "str",
+          damageDice: "1d6"
+        }
       },
       {
-        name: "+1 Magic Dagger of Frost",
-        weight: "1 lb",
-        damageDice: "1d4",
-        damageModifier: "1d6",
-        damageModifierType: "ice"
+        name: "Crossbow Bolt",
+        count: 20,
+        weight: 0.1,
+        weightUnit: "lb",
+        extra: {
+          damageDice: "1d6",
+          attackAttribute: "dex"
+        }
       }
     ]
   },
   {
     name: "Jebth",
     characterClass: "Monk",
-    level: 5
+    level: 5,
+    inventory: [
+      {
+        name: "+1 Magic Dagger of Frost",
+        count: 1,
+        weight: 1,
+        weightUnit: "lb",
+        extra: {
+          attackAttribute: "dex",
+          damageDice: "1d4",
+          damageModifier: "1d6",
+          damageModifierType: "ice"
+        }
+      },
+    ]
+  },
+  {
+    name: "Rickety Crickets",
+    characterClass: "Hobo",
+    level: 3,
   }
 ];
 
